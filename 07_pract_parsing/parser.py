90c2dd7bd8442be28e7bbef8dee5b7f86ecb88dc
CheremiskinGleb <gleb.a.2017@yandex.ru>
2021-06-04 16:42:00 +0300

    Внесены изменения

--- 07_pract_parsing/parser.py
+++ 07_pract_parsing/parser.py
@@ -14,7 +14,7 @@ author = doc.select('span.user-info__nickname')[0].decode_contents().strip()
 title = doc.select('span.post__title-text')[0].decode_contents().strip()
 date = doc.select('span.post__time')[0].decode_contents().strip()
 tags = list(map(lambda x: x.decode_contents().strip(), doc.select('.post__hubs li a')))
-rating = int(doc.select('.voting-wjt__counter')[0].decode_contents().strip())
+rating = str(doc.select('.voting-wjt__counter')[0].decode_contents().strip())
 
 # вывод на экран
 print('Автор:', author)
@@ -27,22 +27,23 @@ print('Рейтинг:', rating)
 comments = []
 for node in doc.select('.comment'):
     text = node.select('.comment__message')[0].decode_contents().strip()
-    rating = int(node.select('.voting-wjt__counter')[0].decode_contents().strip())
+    rating = str(node.select('.voting-wjt__counter')[0].decode_contents().strip())
     author = node.select('.user-info__nickname')[0].decode_contents().strip()
     comments.append({'text': text, 'rating': rating, 'author': author})
 
 # вывод информации по комментариям
 print('Комментариев в статье: ', len(comments))
-print('Самый маленький комментарий:', sorted(comments, key=lambda x: len(x['text']))[0]['text'])
-most_popular = sorted(comments, key=lambda x: x['rating'], reverse=True)[0]
-print('Самый популярный комментарий:', most_popular['text'], 'Рейтинг ', most_popular['rating'])
+if len(comments) > 0:
+    print('Самый маленький комментарий:', sorted(comments, key=lambda x: len(x['text']))[0]['text'])
+    most_popular = sorted(comments, key=lambda x: x['rating'], reverse=True)[0]
+    print('Самый популярный комментарий:', most_popular['text'], 'Рейтинг ', most_popular['rating'])
 
 # самый активный комментатор
-commentators = {}
-for comment in comments:
-    if comment['author'] in commentators:
-        commentators[comment['author']] += 1
-    else:
-        commentators[comment['author']] = 1
-most_active = max(commentators, key=commentators.get)
-print('Самый активный:', most_active, ', комментариев:', commentators[most_active])
+    commentators = {}
+    for comment in comments:
+        if comment['author'] in commentators:
+            commentators[comment['author']] += 1
+        else:
+            commentators[comment['author']] = 1
+    most_active = max(commentators, key=commentators.get)
+    print('Самый активный:', most_active, ', комментариев:', commentators[most_active])
